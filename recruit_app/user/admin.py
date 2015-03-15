78554ddee9421c4801ddf35272aa916f56a94332
Tyler274 <tyler274port@gmail.com>
2015-03-15 17:41:22 -0400

    fffff

--- recruit_app/user/admin.py
+++ recruit_app/user/admin.py
@@ -1,8 +1,8 @@
 from flask_admin.contrib.sqla import ModelView
 from recruit_app.user.models import EveCharacter, EveAllianceInfo, EveApiKeyPair, EveCorporationInfo
-from recruit_app.user.models import User, Role
+from recruit_app.user.models import User, Role, roles_users
 
-from recruit_app.extensions import user_datastore
+from recruit_app.extensions import user_datastore, db
 from flask_security import current_user
 
 
@@ -18,8 +18,12 @@ class AuthenticatedModelView(ModelView):
     column_display_pk = True
 
     def is_accessible(self):
-        # if not Role.query.filter_by(name="admin").first():
-        #             Role.create(name="admin", description="Admin Role")
+        if not Role.query.filter_by(name="admin").first():
+            a = Role.create(name="admin", description="Admin Role")
+            ru = roles_users('1','1')
+            db.session.add(ru)
+            db.session.commit()
+
 
         if current_user.has_role("admin"):
             return True
