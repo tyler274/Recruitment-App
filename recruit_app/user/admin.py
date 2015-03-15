8c54646f3997d53a093b456aaf9e85ba9736d50e
Tyler274 <tyler274port@gmail.com>
2015-03-15 01:37:29 -0400

    grr admin.py

--- recruit_app/user/admin.py
+++ recruit_app/user/admin.py
@@ -21,7 +21,6 @@ class AuthenticatedModelView(ModelView):
         # user_datastore.create_role(name="admin", description="Admin Role")
         u = User.query.filter_by(id=current_user.get_id()).first()
         r = Role.query.filter_by(name="admin").first()
-        user_datastore.add_role_to_user(u, r)
         if u.has_role(r):
             return True
         return False
