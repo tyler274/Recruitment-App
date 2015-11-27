8777f5314d5e5822727a9a8426c69d4fd90f5b02
ggrog <tim@ggrog.com>
2015-11-27 00:18:17 -0600

    Better API error messages

--- recruit_app/user/forms.py
+++ recruit_app/user/forms.py
@@ -45,21 +45,21 @@ class UpdateKeyForm(Form):
         if not initial_validation:
             return False
         if EveManager.check_if_api_key_pair_exist(self.api_id.data):
-            self.api_id.errors.append(u'API key already exists')
+            self.api_id.errors.append(u'API key already registered with this site')
             return False
 
         if not EveApiManager.check_api_is_type_account(self.api_id.data,
                                                        self.api_key.data):
-            self.api_id.errors.append(u'API Key is not an account wide API. Select \"All\" as the character when creating the API key.')
+            self.api_id.errors.append(u'API key is not an account wide API. Select \"All\" as the character when creating the API key.')
             return False
 
         if not EveApiManager.check_api_is_full(self.api_id.data,
                                                self.api_key.data):
-            self.api_id.errors.append(u'API supplied is not a full api key')
+            self.api_id.errors.append(u'API key is not a full API key. Select all items in all categories when creating the key, or use the provided \"Create a full API key\" button to preselect the correct items.')
             return False
         if not EveApiManager.check_api_is_not_expire(self.api_id.data,
                                                      self.api_key.data):
-            self.api_id.errors.append(u'API supplied is not forever')
+            self.api_id.errors.append(u'API key has an expiry date. Please select the "No Expiry" box when creating the API key.')
             return False
         return True
 
