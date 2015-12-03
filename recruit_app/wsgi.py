3a82e204b9da1be6d978d3cb7f2dadfd7eddb2fc
ggrog <tim@ggrog.com>
2015-12-02 20:42:39 -0600

    Maintenance mode

--- recruit_app/wsgi.py
+++ recruit_app/wsgi.py
@@ -2,3 +2,7 @@ __author__ = 'tyler274'
 from recruit_app.app import create_app
 
 app = create_app()
+
+#@app.before_request
+#def maintenance_mode():
+#    return "Site temporarily down for scheduled maintenance.  We'll be back at 04:00 UTC", 503
