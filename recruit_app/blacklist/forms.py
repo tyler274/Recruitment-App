fcca6beed03af2478f5f7b0b8b9ebdc18e245c67
ggrog <tim@ggrog.com>
2015-12-04 22:35:39 -0600

    Cleanup blacklist template/view/search

--- recruit_app/blacklist/forms.py
+++ recruit_app/blacklist/forms.py
@@ -10,27 +10,11 @@ class SearchForm(Form):
 
 
 class BlacklistCharacterForm(Form):
-    character_name = TextAreaField("Character Name",
-                                validators=[Length(min=-1, max=2000, message='Max length %(max)d')])
-
-    main_name = TextAreaField("Main Name",
-                                validators=[Length(min=-1, max=2000, message='Max length %(max)d')])
-
-    corporation = TextAreaField("Corporation",
-                             validators=[Length(min=-1,
-                                                                max=2000,
-                                                                message='Max length %(max)d')])
-
-    alliance = TextAreaField("Alliance", validators=[Length(min=-1,
-                                                                            max=2000,
-                                                                            message='Max length %(max)d')])
-    ip_address = TextAreaField("IP Address", validators=[IPAddress(), Optional()])
-
-    notes = TextAreaField("Notes", validators=[Length(min=-1,
-                                                                      max=2000,
-                                                                      message='Max length %(max)d')])
+    character_name = TextAreaField("Character Name", validators=[Length(min=-1, max=2000, message='Max length %(max)d')])
+    main_name      = TextAreaField("Main Name",      validators=[Length(min=-1, max=2000, message='Max length %(max)d')])
+    corporation    = TextAreaField("Corporation",    validators=[Length(min=-1, max=2000, message='Max length %(max)d')])
+    alliance       = TextAreaField("Alliance",       validators=[Length(min=-1, max=2000, message='Max length %(max)d')])
+    ip_address     = TextAreaField("IP Address",     validators=[IPAddress(), Optional()])
+    notes          = TextAreaField("Notes",          validators=[Length(min=-1, max=2000, message='Max length %(max)d')])
 
     submit = SubmitField(label='Submit Entry')
-
-
-
--- recruit_app/blacklist/views.py
+++ recruit_app/blacklist/views.py
@@ -18,23 +18,28 @@ def blacklist_view(page=1):
     search_form = SearchForm()
     blacklist_form = BlacklistCharacterForm()
 
-    blacklist = BlacklistCharacter.query.paginate(page, current_app.config['MAX_NUMBER_PER_PAGE'], False)
-
+    blacklist = None
+    
     if request.method == 'POST':
-        if current_user.has_role('admin'):
-            if blacklist_form.validate_on_submit():
-                if BlacklistManager.create_entry(blacklist_form, current_user):
-                    flash('Entry Added')
-        else:
-            flash('Not an Admin')
-        if search_form.validate_on_submit():
-            # blacklist = BlacklistCharacter\
-            #     .query\
-            #     .whoosh_search('*' + str(search_form.search.data) + '*')\
-            #     .paginate(page, current_app.config['MAX_NUMBER_PER_PAGE'], False)
-            pass
+        if request.form['submit'] == 'Submit Entry':
+            if current_user.has_role('admin'):
+                if blacklist_form.validate_on_submit():
+                    if BlacklistManager.create_entry(blacklist_form, current_user):
+                        flash('Entry Added')
+            else:
+                flash('Not an Admin')
+        elif search_form.validate_on_submit() and search_form.search.data:
+            blacklist = BlacklistCharacter.query.filter(
+                BlacklistCharacter.name.ilike       ("%" + search_form.search.data + "%")|
+                BlacklistCharacter.main_name.ilike  ("%" + search_form.search.data + "%")|
+                BlacklistCharacter.corporation.ilike("%" + search_form.search.data + "%")|
+                BlacklistCharacter.alliance.ilike   ("%" + search_form.search.data + "%")|
+                BlacklistCharacter.notes.ilike      ("%" + search_form.search.data + "%")|
+                BlacklistCharacter.ip_address.ilike ("%" + search_form.search.data + "%"))\
+                .paginate(1, current_app.config['MAX_NUMBER_PER_PAGE'], False)
 
-    return render_template('blacklist/blacklist.html',
-                           blacklist=blacklist,
-                           search_form=search_form,
-                           blacklist_form=blacklist_form)
+    # Do the main query after POST to catch new entries, or ignore search query
+    if blacklist is None:
+        blacklist = BlacklistCharacter.query.paginate(page, current_app.config['MAX_NUMBER_PER_PAGE'], False)
+
+    return render_template('blacklist/blacklist.html', blacklist=blacklist, search_form=search_form, blacklist_form=blacklist_form)
