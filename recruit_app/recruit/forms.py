81cbd38a19d7bddae709cc35c708280652549a3a
ggrog <tim@ggrog.com>
2016-01-20 14:24:01 -0600

    Revise "the" question

--- recruit_app/recruit/forms.py
+++ recruit_app/recruit/forms.py
@@ -21,21 +21,33 @@ class RequiredIfNot(object):
 
 class HrApplicationForm(Form):
 
-    characters = SelectMultipleField(label="Characters which are applying", validators=[DataRequired()], option_widget=widgets.CheckboxInput(), widget=widgets.ListWidget(prefix_label=False))
+    characters = SelectMultipleField(label="Characters which are applying",
+        validators=[DataRequired()], option_widget=widgets.CheckboxInput(), widget=widgets.ListWidget(prefix_label=False))
 
     alt_application = BooleanField(label="Is this an application for an alt?, leave unchecked if you're not sure")
 
-    thesis                  = TextAreaField("Please give a maximum 2000 character thesis on the meaning of the word \"The\" (not required)", validators=[Length(min=-1, max=2000, message='Max length %(max)d')])
-    how_long                = TextAreaField("How long have you been playing EVE?",                                                           validators=[RequiredIfNot('alt_application'), Length(min=-1, max=2000, message='Max length %(max)d')])
-    notable_accomplishments = TextAreaField("Any notable accomplishments in that time?",                                                     validators=[RequiredIfNot('alt_application'), Length(min=-1, max=2000, message='Max length %(max)d')])
-    corporation_history     = TextAreaField("Walk us through your Corporation history:",                                                     validators=[RequiredIfNot('alt_application'), Length(min=-1, max=2000, message='Max length %(max)d')])
-    why_leaving             = TextAreaField("Why are you leaving your current Corporation?",                                                 validators=[RequiredIfNot('alt_application'), Length(min=-1, max=2000, message='Max length %(max)d')])
-    what_know               = TextAreaField("What do you know about KarmaFleet",                                                             validators=[RequiredIfNot('alt_application'), Length(min=-1, max=2000, message='Max length %(max)d')])
-    what_expect             = TextAreaField("How do you expect daily life in KarmaFleet to be?",                                             validators=[RequiredIfNot('alt_application'), Length(min=-1, max=2000, message='Max length %(max)d')])
-    bought_characters       = TextAreaField("Do you have any alts or other characters? Are any of your characters bought, and if so, which ones?", validators=[RequiredIfNot('alt_application'), Length(min=-1, max=2000, message='Max length %(max)d')])
-    why_interested          = TextAreaField("Why are you interested in joining KarmaFleet?",                                                 validators=[RequiredIfNot('alt_application'), Length(min=-1, max=2000, message='Max length %(max)d')])
-    goon_interaction        = TextAreaField("Have you ever interacted with another member of Goonswarm Federation?",                         validators=[RequiredIfNot('alt_application'), Length(min=-1, max=2000, message='Max length %(max)d')])
-    friends                 = TextAreaField("Do you have any friends in KarmaFleet? If so, who?",                                            validators=[RequiredIfNot('alt_application'), Length(min=-1, max=2000, message='Max length %(max)d')])
+    thesis                  = TextAreaField("KarmaFleet is a new player corporation. If you started playing EVE more than 6 months ago, why are you looking to join a new player corporation? If you are new, what made you want to play EVE in the first place?",
+                                validators=[RequiredIfNot('alt_application'), Length(min=-1, max=2000, message='Max length %(max)d')])
+    how_long                = TextAreaField("How long have you been playing EVE?",
+                                validators=[RequiredIfNot('alt_application'), Length(min=-1, max=2000, message='Max length %(max)d')])
+    notable_accomplishments = TextAreaField("Any notable accomplishments in that time?",
+                                validators=[RequiredIfNot('alt_application'), Length(min=-1, max=2000, message='Max length %(max)d')])
+    corporation_history     = TextAreaField("Walk us through your Corporation history:",
+                                validators=[RequiredIfNot('alt_application'), Length(min=-1, max=2000, message='Max length %(max)d')])
+    why_leaving             = TextAreaField("Why are you leaving your current Corporation?",
+                                validators=[RequiredIfNot('alt_application'), Length(min=-1, max=2000, message='Max length %(max)d')])
+    what_know               = TextAreaField("What do you know about KarmaFleet",
+                                validators=[RequiredIfNot('alt_application'), Length(min=-1, max=2000, message='Max length %(max)d')])
+    what_expect             = TextAreaField("How do you expect daily life in KarmaFleet to be?",
+                                validators=[RequiredIfNot('alt_application'), Length(min=-1, max=2000, message='Max length %(max)d')])
+    bought_characters       = TextAreaField("Do you have any alts or other characters? Are any of your characters bought, and if so, which ones?",
+                                validators=[RequiredIfNot('alt_application'), Length(min=-1, max=2000, message='Max length %(max)d')])
+    why_interested          = TextAreaField("Why are you interested in joining KarmaFleet?",
+                                validators=[RequiredIfNot('alt_application'), Length(min=-1, max=2000, message='Max length %(max)d')])
+    goon_interaction        = TextAreaField("Have you ever interacted with another member of Goonswarm Federation?",
+                                validators=[RequiredIfNot('alt_application'), Length(min=-1, max=2000, message='Max length %(max)d')])
+    friends                 = TextAreaField("Do you have any friends in KarmaFleet? If so, who?",
+                                validators=[RequiredIfNot('alt_application'), Length(min=-1, max=2000, message='Max length %(max)d')])
 
     scale = SelectField("On a scale from 1 to 10, where 1 is pure PvE and 10 is pure PvP, where do you see yourself?",
                         choices=[('0 (Not Sure)', '0 (Not Sure)'),
@@ -50,8 +62,10 @@ class HrApplicationForm(Form):
                                  ('9', '9'),
                                  ('10 (Pure PVP)', '10 (Pure PVP)')])
 
-    favorite_role = TextAreaField("What's your favorite role to play, why?", validators=[RequiredIfNot('alt_application'), Length(min=-1, max=2000, message='Max length %(max)d')])
-    find_out      = TextAreaField("How did you find out about KarmaFleet?",  validators=[RequiredIfNot('alt_application'), Length(min=-1, max=2000, message='Max length %(max)d')])
+    favorite_role = TextAreaField("What's your favorite role to play, why?",
+                        validators=[RequiredIfNot('alt_application'), Length(min=-1, max=2000, message='Max length %(max)d')])
+    find_out      = TextAreaField("How did you find out about KarmaFleet?",
+                        validators=[RequiredIfNot('alt_application'), Length(min=-1, max=2000, message='Max length %(max)d')])
 
     recaptcha = RecaptchaField()
 
